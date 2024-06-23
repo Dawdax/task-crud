2eb5424a37a4e7eaa939bb06f7d780632ed7bbc5
unknown <davidaguilar5034@gmail.com>
2024-06-23 11:57:45 -0600

    Se hicieron todas las funciones para el get, getbyid, create, update y delete, asimismo se agregaron algunas validaciones por si el servidor se cae

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -1,32 +1,71 @@
 import { pool } from "../db.js";
 
+//Visualizar todas las tareas
+const getTasks = async (req, res) => {
+    try {
 
-const getTasks = (req, res) =>{
-    res.send('Obteniendo tareas');
+        const [result] = await pool.query('SELECT * FROM tasks ORDER BY createAt ASC');
+        res.json(result)
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
 }
-const getTask = (req, res) =>{
-    res.send('Obteniendo una tarea');
+//Visualizar una tal sola tarea usando el id
+const getTaskById = async (req, res) => {
+    try {
+        const [result] = await pool.query('SELECT * FROM tasks WHERE id= ?', [req.params.id]);
+        if (result.length === 0) {
+            return res.status(404).json({ message: 'Task not found, error 404' })
+        }
+        res.json(result[0]);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
 }
-const createTask = async (req, res) =>{
-    const {title, description} = req.body
-    const [result] = await pool.query('INSERT INTO tasks(title, description) VALUES (?, ?)', [title, description]);
-    res.json({
-        id: result.insertId,
-        title,
-        description
-    }) 
+//Crear una tarea
+const createTask = async (req, res) => {
+    try {
+        const { title, description } = req.body
+        const [result] = await pool.query('INSERT INTO tasks(title, description) VALUES (?, ?)', [title, description]);
+        res.json({
+            id: result.insertId,
+            title,
+            description
+        })
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+//Actualizar una tarea
+const updateTask = async (req, res) => {
+    try {
+        const { title, description, done } = req.body;
+        const [result] = await pool.query('UPDATE tasks SET title = ? , description = ? , done = ?  WHERE id = ?', [title, description, done, req.params.id]);
+
+        res.json(result);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
 
 }
-const updateTask = (req, res) =>{
-    res.send('Actualizando tarea');
-}
-const deleteTask = (req, res) =>{
-    res.send('Eliminando tarea');
+//Eliminar una tarea
+const deleteTask = async (req, res) => {
+    try {
+        const [result] = await pool.query('DELETE FROM tasks WHERE id = ?', req.params.id);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Task not found' });
+        }
+        return res.sendStatus(204);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+
 }
 
 export {
     getTasks,
-    getTask,
+    getTaskById,
     createTask,
     updateTask,
     deleteTask
